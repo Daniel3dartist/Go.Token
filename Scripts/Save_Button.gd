8a1c7255c69fffaa67433303d9397c7f2c88b59f
Daniel3dartist <danielsobrinho3dartist@gmail.com>
2023-05-11 22:02:27 -0300

    Implemented minor updates to image save buttons.

--- Scripts/Save_Button.gd
+++ Scripts/Save_Button.gd
@@ -6,6 +6,7 @@ var platform = OS.get_name()
 
 func _ready():
 	if platform == "HTML5":
+		self.get_parent().get_node("Dir").visible = false
 		self.icon = load("res://Base_Image/download-install-line-icon-White.png")
 	else:
 		self.icon = load("res://Base_Image/save-icon.png")
--- Scripts/Save_PNG_Token_File_button.gd
+++ Scripts/Save_PNG_Token_File_button.gd
@@ -7,6 +7,7 @@ var platform = OS.get_name()
 func _ready():
 	if platform == "HTML5":
 		self.icon = load("res://Base_Image/download-image-icon-White.png")
+		self.get_parent().get_node("Dir").visible = false
 	else:
 		self.icon = load("res://Base_Image/save-icon.png")
 
